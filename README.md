f29be155ea20100269b3cd10f11a3d27a44b928b
elianacc <ync999@qq.com>
2023-02-16 16:05:59 +0800

    build: 项目结构优化，maven依赖整合优化，增加服务提供者整合模块，redis工具移入通用模块

--- README.md
+++ README.md
@@ -8,11 +8,12 @@
 
 ```apl
 yurayura-cloud
-├── yurayura-cloud-client-business-base ----- 服务消费者包整合（b端）
+├── yurayura-cloud-client-business-base ----- 服务消费者基础包整合（b端）
 ├── yurayura-cloud-client-business-comic ----- 番剧服务消费者（b端）
 ├── yurayura-cloud-client-business-sys ----- 系统服务消费者（b端）
 ├── yurayura-cloud-client-business-user ----- 用户服务消费者（b端）
-├── yurayura-cloud-commons ----- 通用entity、vo、dto、enumerate、exception等整合
+├── yurayura-cloud-commons ----- 通用entity、vo、dto、aspect、enumerate、exception等整合
+├── yurayura-cloud-service-base ----- 服务生产者基础包整合
 ├── yurayura-cloud-service-comic ----- 番剧服务生产者
 ├── yurayura-cloud-service-sys ----- 系统服务生产者
 └── yurayura-cloud-service-user ----- 用户服务生产者
--- yurayura-cloud/yurayura-cloud-service-base/src/main/java/pers/elianacc/yurayura/component/GlobalExceptionResolver.java
+++ yurayura-cloud/yurayura-cloud-service-base/src/main/java/pers/elianacc/yurayura/component/GlobalExceptionResolver.java
@@ -4,13 +4,13 @@ import com.alibaba.fastjson.JSON;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import pers.elianacc.yurayura.bo.MailBo;
-import pers.elianacc.yurayura.util.MailUtil;
-import pers.elianacc.yurayura.vo.ApiResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import pers.elianacc.yurayura.bo.MailBo;
+import pers.elianacc.yurayura.util.MailUtil;
+import pers.elianacc.yurayura.vo.ApiResult;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
